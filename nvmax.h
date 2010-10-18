b3e13fbd5d5b3b87754b96c16863781e70d281d6
wolle <friederichwo@users.noreply.github.com>
2010-10-18 08:54:51 +0000

    at work

--- nvmax.h
+++
@@ -1,7 +0,0 @@
-c-------------------------------------------------------------------
-c  $Id: nvmax.h,v 1.1.1.1 2003/01/13 14:27:03 wolle Exp $
-c
-c  Set max number of components of function vector
-c-------------------------------------------------------------------
-	integer nvmax
-	parameter(nvmax=5)
