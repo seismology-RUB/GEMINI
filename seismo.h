e57223e50ca75cb6de0eda7587737a34fb543578
wolle <friederichwo@users.noreply.github.com>
2010-10-18 08:55:10 +0000

    at work

--- seismo.h
+++
@@ -1,7 +0,0 @@
-c------------------------------------------------------
-c  Header file for seismo procedures
-c  Dimensions in seismodim.h
-c---------------------------------------------------
-	real seismo_dt
-	integer seismo_nsamp,seismo_nslog
-	common/seismo/seismo_dt,seismo_nsamp,seismo_nslog
