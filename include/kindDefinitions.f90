0860eefd645cd63e6ae4519c1e2da4ae2e3d8be5
wolle <wolle@04fb35f3-334d-0410-984c-94bea0f3f75d>
2013-02-18 15:56:16 +0000

    change to GPL licensed version

--- include/kindDefinitions.f90
+++
@@ -1,6 +0,0 @@
-!--------------------------------------------------
-!  Module with kind definitions
-!--------------------------------------------------
- module kindDefinitions
-	integer, parameter :: longint = selected_int_kind(18)
- end module 
